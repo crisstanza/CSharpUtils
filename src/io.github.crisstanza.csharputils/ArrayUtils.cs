dafcf8719d060daca5d5353eb40b148f2c7efd6f
Cris Stanza <crisstanza@gmail.com>
2021-11-17 18:33:34 -0300

    Arrays, stopwatch, performance and http client.

--- src/io.github.crisstanza.csharputils/ArrayUtils.cs
+++ src/io.github.crisstanza.csharputils/ArrayUtils.cs
@@ -4,6 +4,12 @@ namespace io.github.crisstanza.csharputils
 {
 	public class ArrayUtils
 	{
+		public int StringSize(byte[] array)
+		{
+			int i = 0;
+			for (; i < array.Length && array[i] > 0; i++) ;
+			return i;
+		}
 		public string Join(int start, object[] array)
 		{
 			StringBuilder sb = new StringBuilder();
---
+++ src/io.github.crisstanza.csharputils/DiagnosticsUtils.cs
@@ -0,0 +1,68 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace io.github.crisstanza.csharputils
+{
+	public class DiagnosticsUtils
+	{
+		public class CpuDiagnostics
+		{
+			public float InstanceCounter { get; set; }
+			public float ProcessorCounter { get; set; }
+			public string InstanceName { get; set; }
+		}
+
+		public CpuDiagnostics GetCpuDiagnostics()
+		{
+			CpuDiagnostics cpuDiagnostics = new CpuDiagnostics();
+
+			string instanceName = GetInstanceNameForProcessId(Process.GetCurrentProcess().Id);
+
+			PerformanceCounter processCounter = new PerformanceCounter();
+			processCounter.CategoryName = "Process";
+			processCounter.CounterName = "% Processor Time";
+			processCounter.InstanceName = instanceName;
+
+			PerformanceCounter cpuCounter = new PerformanceCounter();
+			cpuCounter.CategoryName = "Processor";
+			cpuCounter.CounterName = "% Processor Time";
+			cpuCounter.InstanceName = "_Total";
+
+			processCounter.NextValue();
+			cpuCounter.NextValue();
+			Thread.Sleep(500);
+
+			cpuDiagnostics.InstanceCounter = processCounter.NextValue();
+			cpuDiagnostics.ProcessorCounter = cpuCounter.NextValue();
+			cpuDiagnostics.InstanceName = instanceName;
+
+			return cpuDiagnostics;
+		}
+
+		private string GetInstanceNameForProcessId(int processId)
+		{
+			Process process = Process.GetProcessById(processId);
+			string processName = Path.GetFileNameWithoutExtension(process.ProcessName);
+
+			PerformanceCounterCategory cat = new PerformanceCounterCategory("Process");
+			string[] instanceNames = cat.GetInstanceNames();
+
+			foreach (string instance in instanceNames)
+			{
+				if (instance.StartsWith(processName))
+				{
+					using (PerformanceCounter cnt = new PerformanceCounter("Process", "ID Process", instance, true))
+					{
+						int val = (int)cnt.RawValue;
+						if (val == processId)
+						{
+							return instance;
+						}
+					}
+				}
+			}
+			return null;
+		}
+	}
+}
