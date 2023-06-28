b6259c52fc890327544f9b5cd26f2982d5e3adce
Cris Stanza <crisstanza@users.noreply.github.com>
2023-06-28 12:57:37 -0300

    Vs. 0.3.0.0 - BooleanUtils

---
+++ src/io.github.crisstanza.csharputils/BooleanUtils.cs
@@ -0,0 +1,10 @@
+﻿namespace io.github.crisstanza.csharputils
+{
+    public class BooleanUtils
+    {
+        public int ToInt(bool value)
+        {
+            return value ? 1 : 0;
+        }
+    }
+}
