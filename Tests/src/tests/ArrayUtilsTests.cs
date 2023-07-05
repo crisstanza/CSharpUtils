1d3050e15ac188b65ecc88c1bd1870bbf2301143
Cris Stanza <crisstanza@users.noreply.github.com>
2023-07-05 11:00:17 -0300

    Vs. 0.3.2.2 - ArrayUtilsTests and ArrayUtils.StringSize/Join null safe

---
+++ Tests/src/tests/ArrayUtilsTests.cs
@@ -0,0 +1,45 @@
+﻿using io.github.crisstanza.csharputils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests
+{
+    [TestClass]
+    public class ArrayUtilsTests
+    {
+        private readonly ArrayUtils arrayUtils = new ArrayUtils();
+
+        [TestMethod]
+        [DataRow(null, 0)]
+        [DataRow(new byte[] { }, 0)]
+        [DataRow(new byte[] { 0 }, 1)]
+        [DataRow(new byte[] { 1, 2, 3 }, 3)]
+        public void Size(byte[] input, int expected)
+        {
+            int output = this.arrayUtils.Size(input);
+            Assert.AreEqual(expected, output);
+        }
+
+        [TestMethod]
+        [DataRow(null, 0)]
+        [DataRow(new byte[] { }, 0)]
+        [DataRow(new byte[] { 0 }, 0)]
+        [DataRow(new byte[] { 1, 0, 2 }, 1)]
+        [DataRow(new byte[] { 1, 2, 3, 0, 0 }, 3)]
+        public void StringSize(byte[] input, int expected)
+        {
+            int output = this.arrayUtils.StringSize(input);
+            Assert.AreEqual(expected, output);
+        }
+
+        [TestMethod]
+        [DataRow(0, null, "")]
+        [DataRow(0, new string[] { "a", "b" }, "ab")]
+        [DataRow(1, new string[] { "a", "b", "c" }, "bc")]
+        [DataRow(10, new string[] { "a", "b", "c" }, "")]
+        public void Join(int inputStart, string[] inputArray, string expected)
+        {
+            string output = this.arrayUtils.Join(inputStart, inputArray);
+            Assert.AreEqual(expected, output);
+        }
+    }
+}
--- src/io.github.crisstanza.csharputils/ArrayUtils.cs
+++ src/io.github.crisstanza.csharputils/ArrayUtils.cs
@@ -10,6 +10,10 @@ namespace io.github.crisstanza.csharputils
         }
         public int StringSize(byte[] array)
         {
+            if (array == null)
+            {
+                return 0;
+            }
             int i = 0;
             for (; i < array.Length && array[i] > 0; i++) ;
             return i;
@@ -17,9 +21,12 @@ namespace io.github.crisstanza.csharputils
         public string Join(int start, object[] array)
         {
             StringBuilder sb = new StringBuilder();
-            for (int i = start; i < array.Length; i++)
+            if (array != null)
             {
-                sb.Append(array[i]);
+                for (int i = start; i < array.Length; i++)
+                {
+                    sb.Append(array[i]);
+                }
             }
             return sb.ToString();
         }
