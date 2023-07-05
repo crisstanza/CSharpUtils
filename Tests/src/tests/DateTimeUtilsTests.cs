6a5ca2fee43d20a43acab3aef97d7cd869b899f9
Cris Stanza <crisstanza@users.noreply.github.com>
2023-07-05 14:01:06 -0300

    Vs. 3.2.3 - DateTimeUtilsTests and DiagnosticsUtilsTests

---
+++ Tests/src/tests/DateTimeUtilsTests.cs
@@ -0,0 +1,18 @@
+﻿using io.github.crisstanza.csharputils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests
+{
+    [TestClass]
+    public class DateTimeUtilsTests
+    {
+        private readonly DateTimeUtils dateTimeUtils = new DateTimeUtils();
+
+        [TestMethod]
+        public void Now()
+        {
+            string output = this.dateTimeUtils.Now();
+            Assert.IsNotNull(output);
+        }
+    }
+}
---
+++ Tests/src/tests/DiagnosticsUtilsTests.cs
@@ -0,0 +1,18 @@
+﻿using io.github.crisstanza.csharputils;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests
+{
+    [TestClass]
+    public class DiagnosticsUtilsTests
+    {
+        private readonly DiagnosticsUtils diagnosticsUtils = new DiagnosticsUtils();
+
+        [TestMethod]
+        public void Now()
+        {
+            DiagnosticsUtils.CpuDiagnostics output = this.diagnosticsUtils.GetCpuDiagnostics();
+            Assert.IsNotNull(output);
+        }
+    }
+}
