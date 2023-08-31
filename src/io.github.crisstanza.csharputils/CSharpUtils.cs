d8f8c6073f81aa13e0314b8dcb20984ce02b147b
Cris Stanza <crisstanza@users.noreply.github.com>
2023-08-31 15:03:10 -0300

    Vs. 0.3.3.0 - x64 and show version

---
+++ src/io.github.crisstanza.csharputils/CSharpUtils.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace src.io.github.crisstanza.csharputils
+{
+	public class CSharpUtils
+	{
+		public static string Version()
+		{
+			return Assembly.GetExecutingAssembly().GetName().Version.ToString();
+		}
+	}
+}
