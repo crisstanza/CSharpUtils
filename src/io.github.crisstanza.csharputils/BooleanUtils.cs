d3109f5bab9f31c210d049270a81d5d5c76ccf3e
Cris Stanza <crisstanza@users.noreply.github.com>
2023-08-31 16:43:24 -0300

    Vs. 0.3.3.1 - ToInt(bool? value)

--- src/io.github.crisstanza.csharputils/BooleanUtils.cs
+++ src/io.github.crisstanza.csharputils/BooleanUtils.cs
@@ -1,14 +1,14 @@
 ﻿namespace io.github.crisstanza.csharputils
 {
-    public class BooleanUtils
-    {
-        public bool FromInt(int value)
-        {
-            return value == 1;
-        }
-        public int ToInt(bool value)
-        {
-            return value ? 1 : 0;
-        }
-    }
+	public class BooleanUtils
+	{
+		public bool FromInt(int value)
+		{
+			return value == 1;
+		}
+		public int ToInt(bool? value)
+		{
+			return value == null ? 0 : value.Value ? 1 : 0;
+		}
+	}
 }
