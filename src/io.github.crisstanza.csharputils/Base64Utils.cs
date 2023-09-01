2ee8a0a76d7facb524b556ca6a291d60252da929
Cris Stanza <crisstanza@users.noreply.github.com>
2023-09-01 10:32:50 -0300

    Vs. 0.3.3.2 - fix invalid base64 (space/+)

--- src/io.github.crisstanza.csharputils/Base64Utils.cs
+++ src/io.github.crisstanza.csharputils/Base64Utils.cs
@@ -2,15 +2,34 @@
 
 namespace io.github.crisstanza.csharputils
 {
-    public class Base64Utils
-    {
-        public string FromByteArray(byte[] array)
-        {
-            return array == null ? null : Convert.ToBase64String(array);
-        }
-        public byte[] ToByteArray(string value)
-        {
-            return value == null ? null : Convert.FromBase64String(value);
-        }
-    }
+	public class Base64Utils
+	{
+		public string FromByteArray(byte[] array)
+		{
+			return array == null ? null : Convert.ToBase64String(array);
+		}
+		public byte[] ToByteArray(string value, bool fixInput)
+		{
+			if (value == null)
+			{
+				return null;
+			}
+			else if (fixInput)
+			{
+				return Convert.FromBase64String(this.FixInput(value));
+			}
+			else
+			{
+				return Convert.FromBase64String(value);
+			}
+		}
+		public byte[] ToByteArray(string value)
+		{
+			return ToByteArray(value, false);
+		}
+		private string FixInput(string value)
+		{
+			return value.Replace(' ', '+');
+		}
+	}
 }
